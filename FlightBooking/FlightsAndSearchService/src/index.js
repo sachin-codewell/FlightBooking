2042cdfd18ad90ab47d979e04ad507a1ffd5b98c
Sachin Yadav <sny2771999@gmail.com>
2023-11-20 13:29:17 +0530

    airplane model added

--- FlightBooking/FlightsAndSearchService/src/index.js
+++ FlightBooking/FlightsAndSearchService/src/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const appRoutes = require('./routes/index')
 
-const {City} = require('./models/index')
+const {City,Airport} = require('./models/index')
 const { ServerConfig,LoggerConfig:logger } = require('./config/index');
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use('/api',appRoutes)
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async()=>{
     logger.log('info',`server is runnign fine on port ${ServerConfig.PORT}`)
+    const airports = await Airport.findAll({
+        attributes:['name']
+    });
+    console.log(airports);
 })
